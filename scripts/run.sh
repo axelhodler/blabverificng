ab9c0bc23780b216ac2c729d976e0df28290bda5
Axel Hodler <derxor@gmail.com>
2017-04-14 20:48:38 +0200

    reduce duplication

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,18 +5,20 @@ CONFIG_CONTENTS=`./scripts/prepare_contract.sh`
 CONFIG_CONTENTS_ARRAY=($CONFIG_CONTENTS)
 cd ..
 
+CONFIG_FILE=src/app/config.ts
+
 ADDRESS=${CONFIG_CONTENTS_ARRAY[0]}
-sed -i '' 's#_CONTRACT_ADDRESS_#'$ADDRESS'#g' src/app/config.ts
+sed -i '' 's#_CONTRACT_ADDRESS_#'$ADDRESS'#g' $CONFIG_FILE
 
 ABI=${CONFIG_CONTENTS_ARRAY[1]}
-sed -i '' 's#_CONTRACT_ABI_#'$ABI'#g' src/app/config.ts
+sed -i '' 's#_CONTRACT_ABI_#'$ABI'#g' $CONFIG_FILE
 
 if [ "$1" == "e2e" ]; then
   ng e2e
   docker ps | tail -n 1 | awk '{print $1}' | xargs docker kill
-  git checkout src/app/config.ts
+  git checkout $CONFIG_FILE
 else
-  trap "git checkout src/app/config.ts" SIGINT
+  trap "git checkout $CONFIG_FILE" SIGINT
   ng serve
   docker ps | tail -n 1 | awk '{print $1}' | xargs docker kill | > /dev/null
 fi
