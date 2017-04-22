926618f1bd79d2f829a840d49e81f41a8b8f4cd0
Axel Hodler <derxor@gmail.com>
2017-04-22 21:04:19 +0200

    does not belong to release

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,6 +8,7 @@ npm run build
 find . -maxdepth 1 ! -path . -not -name dist -not -name .git -not -name scripts -exec rm -rf {} \;
 mv dist/* .
 rm -rf dist/
+rm -rf scripts/
 git add .
 git commit -m "release"
 git push origin gh-pages --force
