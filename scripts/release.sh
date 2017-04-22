985b4979addc0b9938b89e029b5e2f2934da3f95
Axel Hodler <derxor@gmail.com>
2017-04-22 20:53:37 +0200

    create release scripts

---
+++ scripts/release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+git branch -D gh-pages
+git checkout -b gh-pages
+npm run build
+find . -maxdepth 1 ! -path . -not -name dist -not -name .git -not -name scripts -exec rm -rf {} \;
+mv dist/* .
+rm -rf dist/
+git add .
+git commit -m "release"
+git push origin gh-pages --force
+git checkout master
