b16bdabbcfeb34b33c0aff24bfd3ca3733e14a52
Axel Hodler <derxor@gmail.com>
2017-05-03 07:05:55 +0200

    add jan-paul

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,7 +1,7 @@
 import {InMemoryDbService} from "angular-in-memory-web-api";
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    let reports = [{
+    const reports = [{
       id: '0x60419d353117a1ad25e163afc5ea37fcea09705cb0fa1725547cfc7e808e86db',
       content: 'jochen2016',
       submitter: 'Jochen Kaßberger',
@@ -12,12 +12,15 @@ export class InMemoryDataService implements InMemoryDbService {
       submitter: 'Axel Hodler',
       verifierCount: 1
     }];
-    let members = [{
+    const members = [{
       id: '0x1fe455ff68b5ce672f33ea1a12ab51ff3edc3362',
       name: 'Axel Hodler'
     }, {
       id: '0xf2f2581684fd5b7a0e84b3cdfa4a842a0fcce746',
       name: 'Jochen Kaßberger'
+    }, {
+      id: '0x21eA3453B22Ea6d6ee6e3dA9D00110e6265280E3',
+      name: 'Jan-Paul Buchwald'
     }];
     return {members, reports};
   }
